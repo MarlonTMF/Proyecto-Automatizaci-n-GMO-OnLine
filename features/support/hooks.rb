cd229840e695b32d671b3c30971b13c3b7d638cd
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2025-11-26 15:07:17 -0400

    Hooks Actualizado

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,33 @@
+require 'net/http'
 
-After do 
+
+# Cerrar solo al final de toda la suite o por etiqueta
+After do |scenario|
+  # Solo cerrar si el escenario falló o tiene etiqueta @reset
+  if scenario.failed? || scenario.source_tag_names.include?('@reset')
     Capybara.current_session.driver.quit
+  end
 end
 
 Before '@maximize' do
   page.driver.browser.manage.window.maximize
+end
+
+Before do
+  unless internet_available?
+    raise 'No hay conexión a Internet. Deteniendo pruebas.'
+  end
+end
+
+def internet_available?
+    return false unless Capybara.app_host
+  begin
+    uri = URI(Capybara.app_host)
+    response = Net::HTTP.get_response(uri)
+    response.is_a?(Net::HTTPSuccess) || response.is_a?(Net::HTTPRedirection)
+  rescue SocketError, Errno::ECONNREFUSED
+    false
+  end
 end
 
 AfterStep('@slow') do
