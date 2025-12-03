9e77e1ddd3588ab47c5ea29ac1a29b835d22b1eb
Walter Rocha <walterrochasevilla@gmail.com>
2025-12-03 13:41:25 -0400

    limpieza de entorno

--- features/step_definitions/receipt_steps.rb
+++ features/step_definitions/receipt_steps.rb
@@ -1,5 +1,5 @@
 require_relative '../pages/ReceiptPage'
-require_relative '../pages/BillingPage' # Necesario para reusar llenado de forms
+require_relative '../pages/BillingPage'
 
 def receipt_page
   @receipt_page ||= ReceiptPage.new
@@ -9,17 +9,13 @@ def billing_page
   @billing_page ||= BillingPage.new
 end
 
-# Reusamos steps existentes, pero si hay nuevos:
 
 Then(/^I should see the correct billing and shipping information on the receipt:$/) do |table|
   receipt_page.verify_billing_shipping_info(table)
 end
 
 And(/^I should see the correct order amounts on the receipt robustly:$/) do |table|
-  # Pasamos las variables capturadas por el Helper (@scenario_variables)
-  # Cucumber las guarda como variables de instancia en el World
-  
-  # Truco: Recolectar variables del contexto actual para pasarlas a la pagina
+
   context = {}
   instance_variables.each do |var|
     next unless var.to_s.start_with?('@')
